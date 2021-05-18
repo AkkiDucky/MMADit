7a4c402ddd48961f45775ffa94d2a9f303a95b11
AkkiDucky <62772803+AkkiDucky@users.noreply.github.com>
2021-05-18 11:10:36 +0300

    Update start.py

--- 6 работа/start.py
+++ 6 работа/start.py
@@ -4,10 +4,6 @@ from sklearn import linear_model
 import matplotlib.pyplot as plt
 import functions as func
 from enum import Enum
-class Color(Enum):
-    RED = 0
-    GREEN = 1
-    BLUE = 2
 
 
 func.compress_image('image.jpg', bits_per_channel=5, polynom=4)
